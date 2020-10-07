5401271974c32690061ae7d882f1a51ef51154fc
Anton Maryn <tonysmaryn@gmail.com>
2020-10-07 20:53:38 +0300

    feat: complite dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ if (!Array.isArray(members)) return false;
+ let dreamTeam = '';
+ members.forEach(member => {
+   if(typeof(member) === 'string'){
+     dreamTeam += member.match(/[a-zA-Z]{1}/);
+   }
+ });
+ return dreamTeam.toUpperCase().split('').sort().join('');
 };
