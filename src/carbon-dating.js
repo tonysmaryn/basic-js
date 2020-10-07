eeefcdb05cc7ff9d478c7c5da7e0b90235a14731
Anton Maryn <tonysmaryn@gmail.com>
2020-10-07 20:29:28 +0300

    feat: complite count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) !== "string" || isNaN(Number(sampleActivity)) || Number(sampleActivity) <= 0 || Number(sampleActivity) >= 15)
+    return false;
+  let sA = Number(sampleActivity);
+  let t = Math.abs((Math.log(sA / MODERN_ACTIVITY) * HALF_LIFE_PERIOD) / 0.693);
+  return Math.ceil(t)
 };
